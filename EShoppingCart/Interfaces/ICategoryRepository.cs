067e85d1e7a0e798acf784347fa28ffb2ffa4d61
Melisha <mslafaber1@gmail.com>
2020-08-31 09:14:15 +0930

    Created a MVC application and Created Item and Category Models. Then Created Repositories for each model.

---
+++ EShoppingCart/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using EShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShoppingCart.Interfaces
+{
+    interface ICategoryRepository
+    {
+        //This property is used to hold all categories.
+        IEnumerable<Category> Categories { get; }
+    }
+}
---
+++ EShoppingCart/Interfaces/IItemRepository.cs
@@ -0,0 +1,19 @@
+﻿using EShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShoppingCart.Interfaces
+{
+    interface IItemRepository
+    {
+        //This first property will get all the Items
+        IEnumerable<Item> Items { get; }
+        //The below second property is used to get all preferred Items which will be shown in the front page of the EShoppinCart application
+        IEnumerable<Item> PreferredItems { get; }
+        //Below method is used to get the relevant Item by its Id
+        Item GetItemById(int itemId);
+
+    }
+}
