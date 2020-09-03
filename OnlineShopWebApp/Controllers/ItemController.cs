cefac193b6362e1ae100054f76e6c7cb49ce5d74
Melisha <mslafaber1@gmail.com>
2020-09-04 02:35:25 +0930

    Created Item Controller. Created Views for Item as List.cshtml and edited the word file

---
+++ OnlineShopWebApp/Controllers/ItemController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class ItemController:Controller
+    {
+        //Bring the repositories into the class to use the data from the repositories, by creating read only fields for the interfaces
+        private readonly IItemRepository _itemRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        //Below constructor will instantiate interfaces above by injecting
+        public ItemController(IItemRepository itemRepository, ICategoryRepository categoryRepository)
+        {
+            _itemRepository = itemRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        //action methods are as below
+
+        public ViewResult List() 
+        {
+            return View(_itemRepository.GetAllItems);
+        }
+    }
+}
