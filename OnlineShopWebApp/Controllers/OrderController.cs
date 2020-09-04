49710c02b3c02a920fa02b0ddfa83964fd77a86e
Melisha <mslafaber1@gmail.com>
2020-09-04 20:19:46 +0930

    Create the Order Controller. Created a new folder named Order in the Views folder and created a new view called Checkout and created the form the user fills when checking out. Added checkout button in the shopping cart with the relevant controllers.  Added checkout action for the post method in the Order Controller with the required validations. Created the checkout complete action in order controller and created the CheckoutComplete view in the Order folder under views. Added Input validation on the form inputs in the Order model class. Added Input validation error message displaying to the checkout view template

---
+++ OnlineShopWebApp/Controllers/OrderController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class OrderController:Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        //below will be processed only when the form is submitted
+        [HttpPost]
+
+        //below method processes the order
+        public IActionResult Checkout(Order order)
+        {
+            //get all the items from the shopping cart
+            _shoppingCart.ShoppingCartItems = _shoppingCart.GetShoppingCartItems();
+
+            //validate if there is at least one item in the shopping cart
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your shopping cart is empty");
+            }
+
+            //check if the cart is valid or not/all the data is bound correctly or not
+            if (ModelState.IsValid)
+            {
+                //create the order after validating
+                _orderRepository.CreateOrder(order);
+                //clear the cart
+                _shoppingCart.ClearCart();
+                //user will be sent to the checkout complete page
+                return RedirectToAction("CheckoutComplete");
+            }
+
+            //if the form was not validated then the user will get the view with the data that the user has already added
+            return View(order);
+        }
+
+        //below action will display a message
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Thank you for your purchase!! Hope you enjoy our Products.";
+            return View();
+        }
+    }
+}
--- OnlineShopWebApp/Models/Order.cs
+++ OnlineShopWebApp/Models/Order.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,19 +10,50 @@ namespace OnlineShopWebApp.Models
     public class Order
     {
         //all the below properties show the information of the user
+
+        [BindNever]
         public int OrderId { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your First Name")]
+        [Display(Name = "First Name")]
+        [StringLength(30)]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Last Name")]
+        [Display(Name = "Last Name")]
+        [StringLength(60)]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Address")]
+        [Display(Name = "Street Address")]
+        [StringLength(150)]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your City Name")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your State in Abbreviation Form")]
+        [StringLength(3, MinimumLength = 2)]
         public string State { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Zip Code")]
+        [StringLength(5, MinimumLength = 4)]
+        [Display(Name = "Zip Code")]
         public string ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
 
         public List<OrderDetail> OrderDetails { get; set; }
+
         //Total amount due
+        [BindNever]
         public decimal OrderTotal { get; set; }
+
         //Time when the order was created
+        [BindNever]
         public DateTime OrderPlaced { get; set; }
     }
 }
