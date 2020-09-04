cc61342a74f06512ded0c2a15f2b3448c1efbe9d
Melisha <mslafaber1@gmail.com>
2020-09-04 09:44:10 +0930

    Created Partial view that is used by multiple views but not globally shared. A partial view(_itemCard.cshtml)  was created for the listing all items since the same view is used when rendering all items and a section of items. Then that partial view(_itemCard.cshtml) was called in the List view template. Created the home controller. Created HomeViewModel which will get the Items on sale and render to the home controller which is displayed on the view. Created a view for the home controller to send the data to the index page in home views. _Carousel.cshtml was created as a partial view so that it is called to the home view. Edited the home page to show a slider using partial views and to show the items which are on sale. Edited the word file accordingly with the steps taken

---
+++ OnlineShopWebApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+using OnlineShopWebApp.ViewModels;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IItemRepository _itemRepository;
+
+        //inject the repository into a constructor
+        public HomeController(IItemRepository itemRepository) 
+        {
+            _itemRepository = itemRepository;
+        }
+
+        //returns the Items on sale
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                ItemOnSale = _itemRepository.GetItemsOnSale
+            };
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ OnlineShopWebApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using OnlineShopWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.ViewModels
+{
+    public class HomeViewModel
+    {
+        //folowwing property returns the Items on sale which is used in home controller to pass to the view.
+        public IEnumerable<Item> ItemOnSale { get; set; }
+    }
+}
