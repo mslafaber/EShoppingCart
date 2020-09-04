6e05bd034cb08f449e0449e847e6f6e279132177
Melisha <mslafaber1@gmail.com>
2020-09-04 12:23:52 +0930

    Created a new folder for components and added the shopping cart component with relevant method. And then created default.cshtml views, under the shopping cart summary folder which was create under shared > Components folder. Then added this shopping cart summary view component in the _Layout view so that it is accessible throughout the application.

---
+++ OnlineShopWebApp/Components/ShoppingCartSummary.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+using OnlineShopWebApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Components
+{
+    //class inherits from View componenet base class
+    public class ShoppingCartSummary : ViewComponent
+    {
+        //bring in an instance of the shopping class
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        //below method is Invoked automatically by asp
+        public IViewComponentResult Invoke()
+        {
+            //get items from the shopping cart
+            _shoppingCart.ShoppingCartItems = _shoppingCart.GetShoppingCartItems();
+
+            //instance of the view model created before
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            //view that is returned is of type IViewComponent
+            return View(shoppingCartViewModel);
+        }
+    }
+}
